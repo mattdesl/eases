b874f5de1f1626ea0d0f26c8df15dc0a44ebc14d
mattdesl <dave.des@gmail.com>
2015-10-07 11:45:35 -0400

    use consistent spacing and style

--- test.js
+++ test.js
@@ -2,18 +2,18 @@ var eases = require('./')
 var test = require('tape').test
 
 var outsideRange = [
-    'elasticIn','elasticOut','elasticInOut',
-    'backIn','backOut','backInOut',
-    'bounceIn','bounceOut','bounceInOut',
+  'elasticIn', 'elasticOut', 'elasticInOut',
+  'backIn', 'backOut', 'backInOut',
+  'bounceIn', 'bounceOut', 'bounceInOut',
 ]
 
-test('eases return number between 0 and 1', function(t) {
-	for (var k in eases) {
-		var f = eases[k]
-        if (outsideRange.indexOf(k) === -1) {
-            t.equal(f(1), 1, k)
-            t.equal(f(0), 0, k)
-        }
-	}
-	t.end()
+test('eases return number between 0 and 1', function (t) {
+  for (var k in eases) {
+    var f = eases[k]
+    if (outsideRange.indexOf(k) === -1) {
+      t.equal(f(1), 1, k)
+      t.equal(f(0), 0, k)
+    }
+  }
+  t.end()
 })
